526fa6757f3eb7263894a6f2178c2d28750cf668
Harsh Jha <Harshwork5546@gmail.com>
2025-08-19 16:07:01 +0530

    Learned Variables and constant

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int parthScore; //Variable declaration
+    parthScore = 25; // intializaton
+
+    const int uid = 123021;
+    
+    cout << "Hello with chai" << endl;
+    return 0;
+}
